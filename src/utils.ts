e9da1c51533ee68ccc89604eae2dcc0ab3134b7f
duan <wddwyss@gmail.com>
2020-10-13 23:07:49 +0900

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -14,3 +14,61 @@ export const runParser = <A>(
     p(stream(s.split(''))),
     E.map(a => a.value)
   )
+
+/**
+ * * Backspace is replaced with `\b`
+ * * Form feed is replaced with `\f`
+ * * Newline is replaced with `\n`
+ * * Carriage return is replaced with `\r`
+ * * Tab is replaced with `\t`
+ * * Double quote is replaced with `\"`
+ * * Backslash is replaced with `\\`
+ */
+export const JSONStringEscapesParser = pipe(
+  C.char('\\'),
+  P.chain(() =>
+    pipe(
+      // impl with recursion
+      pipe(
+        C.char('b'),
+        P.map(() => '\b')
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('f'),
+          P.map(() => '\f')
+        )
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('n'),
+          P.map(() => '\n')
+        )
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('r'),
+          P.map(() => '\r')
+        )
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('t'),
+          P.map(() => '\t')
+        )
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('"'),
+          P.map(() => '"')
+        )
+      ),
+      P.alt(() =>
+        pipe(
+          C.char('\\'),
+          P.map(() => '\\')
+        )
+      )
+    )
+  )
+)
